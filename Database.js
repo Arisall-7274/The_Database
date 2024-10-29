25bb69c84ed0af644d4e03fc0efee7df3c3db59b
Arisall-7274 <andreyardashew@gmail.com>
2024-10-29 23:13:43 +0300

    Add files via upload

---
+++ Database.js
@@ -0,0 +1,34 @@
+fetch('https://arisall-7274.github.io/The_Database/')
+    .then(res => console.log(res.json()))
+    // .then(data => console.log(data))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     if(res.ok){
+//         return res.json()
+//     } else {
+//         console.log("ERROR")
+//         throw Error
+//     }
+// })
+// .then(data => 
+//     document.getElementById('root').innerHTML =
+//     JSON.stringify(data)
+//     ).catch(error => {
+//         console.log(error)
+//     })
